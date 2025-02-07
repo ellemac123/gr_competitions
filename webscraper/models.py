d78551c90f2ea9d45904c0a75926e9d5e2b5c430
ellemac123 <laura.macaluso.12@cnu.edu>
2025-02-07 13:25:10 +0000

    create contest model and make initial migration

--- webscraper/models.py
+++ webscraper/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Contest(models.Model): 
+    """
+    This will be used to store the giveaway information for giveaways that have been entered
+    or were attempted to have been entered.
+    """
+    giveaway_id = models.IntegerField()
+    book_name = models.CharField(max_length=260)
+    author = models.CharField(max_length=260)
+    geo_entered = models.CharField(max_length=2, default='US')
+    entered_date = models.DateTimeField("date entered")
+    giveaway_ends_data = models.DateTimeField("giveaway end date")
+    entered = models.BooleanField(default=False)
+    format = models.CharField(max_length=20, default="print")
